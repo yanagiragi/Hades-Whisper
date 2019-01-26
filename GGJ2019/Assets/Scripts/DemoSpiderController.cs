2e9c092fe462e54f9c1d427b583fc44a8960baaa
Kevin Chang <chile109@gmail.com>
2019-01-26 16:11:34 +0800

    [update] demo spider dancing

---
+++ GGJ2019/Assets/Scripts/DemoSpiderController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemoSpiderController : MonoBehaviour
+{
+    
+    public enum EnemyaniType
+    {
+        attack,
+        jump,
+        up,
+
+    }
+    public EnemyaniType animationType;
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        Debug.Log(animationType.ToString());
+        animator.Play(animationType.ToString());
+    }
+
+}
+
