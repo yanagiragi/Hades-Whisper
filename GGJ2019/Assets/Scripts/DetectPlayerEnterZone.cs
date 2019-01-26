dc10dea6705d85f3deff6b4dbe4f599bb4741069
yanagiragi <qaz90473@gmail.com>
2019-01-26 16:53:28 +0800

    Add Level Blocking Mechanisms

---
+++ GGJ2019/Assets/Scripts/DetectPlayerEnterZone.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectPlayerEnterZone : MonoBehaviour
+{
+    public int index = -1;
+
+    public GameObject wallCollider;
+
+    public GlobalLevelManager globalLevelManager;
+
+    public void Start()
+    {
+        if(index == -1)
+            Debug.LogError("Warning on unmodified index!");
+
+        wallCollider.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            wallCollider.SetActive(true);
+            this.enabled = false;
+            globalLevelManager.InvokeEnterZone(index);
+        }
+    }
+}
---
+++ GGJ2019/Assets/Scripts/GlobalLevelManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalLevelManager : MonoBehaviour
+{
+    public Light[] levelOneLights;
+    public Light[] levelTwoLights;
+    public Light[] levelThreeLights;
+
+    public GameObject[] levelOneEmmisiveGameObjects;
+    public GameObject[] levelTwoEmmisiveGameObjects;
+    public GameObject[] levelThreeEmmisiveGameObjects;
+
+    public Material placeHolderMaterial;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void InvokeEnterZone(int index)
+    {
+        // ${index} zone means we leave zone ${index} and need to turn off zone ${index}'s lights
+        switch (index)
+        {
+            case 1:
+                for(int i = 0; i < levelOneLights.Length; ++i)
+                {
+                    levelOneLights[i].enabled = false;
+                }
+                for (int i = 0; i < levelOneEmmisiveGameObjects.Length; ++i)
+                {
+                    levelOneEmmisiveGameObjects[i].GetComponent<Renderer>().material = placeHolderMaterial;
+                }
+                    break;
+            case 2:
+                for (int i = 0; i < levelTwoLights.Length; ++i)
+                {
+                    levelTwoLights[i].enabled = false;
+                }
+                break;
+            case 3:
+                for (int i = 0; i < levelThreeLights.Length; ++i)
+                {
+                    levelThreeLights[i].enabled = false;
+                }
+                break;
+            default:
+                Debug.LogError("Error Index of Enter Zone: " + index);
+                break;
+        }
+    }
+}
