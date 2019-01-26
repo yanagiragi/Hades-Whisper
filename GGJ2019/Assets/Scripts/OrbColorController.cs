19bc84d1f5a72d7f5e9cb5ff205ed1c3d9cc87f5
Kevin Chang <chile109@gmail.com>
2019-01-27 00:49:23 +0800

    [Add] Orb color controller

---
+++ GGJ2019/Assets/Scripts/OrbColorController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbColorController : MonoBehaviour
+{
+    void OnEnable()
+    {
+        Renderer rend = GetComponent<Renderer>();
+        Light light = GetComponentInChildren<Light>();
+
+        rend.material.shader = Shader.Find("_Color");
+        rend.material.SetColor("_Color", light.color);
+    }
+}
